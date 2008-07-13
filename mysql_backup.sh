7ff96340ba71672cad1bf94cc7045aee52ca8140
Andreas Schwarz <sianscwa@hawo-net.de>
2008-07-13 14:16:35 +0200

    use logger to write to syslog check if password was set

--- mysql_backup.sh
+++ mysql_backup.sh
@@ -9,14 +9,33 @@
 DBAdminPass=${1}
 BackupPath=${2}
 
+# check if pass was set
+if [ -z "${DBAdminPass}" ]; then
+	builtin echo "no password"
+	exit 1
+fi
+
+# check if path was set
 if [ -z "${BackupPath}" ]; then
 	builtin echo "no backup path"
 	exit 1
 fi
+
+# check if path is a directory
 if ! [ -d "${BackupPath}" ]; then
 	builtin echo "backup path isn't a directory"
 	exit 1
 fi
 
+# check if logger is executable
+if ! [ -x "/usr/bin/logger" ]; then
+        builtin echo "Missing logger"
+        exit 1
+fi
+
+/usr/bin/logger "$0: start mysql backup on $HOSTNAME by $USER($UID)"
+
 mysqldump -uroot -hlocalhost '$DBAdminPass' --events --comments -R --all-databases > $BackupPath/databases_$(date +%A).sql
+
+/usr/bin/logger "$0: end mysql backup on $HOSTNAME by $USER($UID)"
 
