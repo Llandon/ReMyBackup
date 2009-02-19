8b3d2f7608c9fa5ed4f3424e970b1142e0d3a46e
Andreas Schwarz <sianscwa@hawo-net.de>
2009-02-19 22:11:49 +0100

    combine full and file per database backup add some argument checking

--- mysql_backup.sh
+++ mysql_backup.sh
@@ -3,7 +3,7 @@
 ##
 ## Keep it small & simple MySQL backup skript
 ##
-## 2008 Andreas Schwarz
+## 2008-2009 Andreas Schwarz
 ##
 
 DBAdmin=${1}
@@ -12,6 +12,14 @@ DBAdminPass=${3}
 BackupPath=${4}
 
 IGNORE="mysql|information_schema"
+ALLOPT="--comments -R"
+SEPOPT="--comments -R"
+
+# check if all arguments where set
+if [ -z "${DBAdmin}" ] || [ -z "${DBHOST}" ] || [ -z "${DBAdminPass}" ] || [ -z "$BackupPath" ]; then
+	builtin echo "no all arguments where set $0 [DB-Admin] [DB-Host] [DB-Admin-Ppassword] [Backup path]"
+	exit 1
+fi
 
 # check if all dependencies are met 
 if ! [ -x "/usr/bin/logger" ] || ! [ -x "/usr/bin/mysql" ] || ! [ -x "/usr/bin/mysqldump" ]; then
@@ -22,15 +30,32 @@ fi
 # check if BackupPath is a writeable directory
 if [ ! -d $BackupPath ] || [ ! -w $BackupPath ]; then /usr/bin/logger "$0 - invalid or not writeable backup path"; exit 1; fi
 
+/usr/bin/logger "$0 - database backup begins on $HOSTNAME by $USER($UID)";
+
+# all databases backup ########################################################
+
+/usr/bin/logger "$0 - backup all databases on $HOSTNAME"
+if [[ $? == 0 ]]; then
+	/usr/bin/logger "$0 - backup from databases finished"
+else
+	/usr/bin/logger "$0 - backup from databases failed"
+fi
+
+mysqldump -uroot -hlocalhost '$DBAdminPass' $ALLOPT --all-databases > $BackupPath/databases_$(date +%A).sql
+
+# one file per database backup ################################################
+
 # list of databases without ingored ones
 DBS="$(/usr/bin/mysql -u$DBAdmin -h$DBHost -p$DBAdminPass -Bse 'show databases' | /bin/grep -Ev $IGNORE)"
 
-/usr/bin/logger "$0 - database backup begins on $HOSTNAME by $USER($UID)";
-
 for DB in $DBS; do
 	/usr/bin/logger "$0 - backup from database $DB start"
-	/usr/bin/mysqldump -u$DBAdmin -h$DBHost -p$DBAdminPass --events --comments -R $DB > $BackupPath/database_${DB}_$(date +%A).sql
-	/usr/bin/logger "$0 - backup from database $DB finished"
+	/usr/bin/mysqldump -u$DBAdmin -h$DBHost -p$DBAdminPass $SEPOPT -R $DB > $BackupPath/database_${DB}_$(date +%A).sql
+	if [[ $? == 0 ]]; then
+		/usr/bin/logger "$0 - backup from database $DB finished"
+	else
+		/usr/bin/logger "$0 - backup from database $DB failed"
+	fi
 done
 
 /usr/bin/logger "$0 - database backup finished";
